0d6eb045978abc2f6ce1eadad270d5c075593e63
vanh6803 <anhnv6083@gmail.com>
2023-10-08 22:18:27 +0700

    init project

---
+++ REDME.md
@@ -0,0 +1,7 @@
+## Backend F-market 
+
+# installation
+
+- git clone: https://github.com/vanh6803/MD_04_Fmarket_Backend.git
+- npm install
+- create file .env
---
+++ config/ConnectDB.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.URL_MONGODB + "/FMarket")
+  .then(() => console.log("connect successfully ✅ !!!"))
+  .catch((err) => console.log("connect failed ❌: ", err));
+
+module.exports = {
+  mongoose,
+};
