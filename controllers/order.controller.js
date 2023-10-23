469145dbebfbe389f3792170aabc0fb51c9f2af5
nguyen chi thuan <thuan0936a@gmail.com>
2023-10-23 11:08:26 +0700

    thuan 23/10 create list order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,4 +1,53 @@
-let md = require('../models/Orders');
+let mdOrder = require('../models/Orders');
+let mdOrderDetail = require('../models/OrderDetail');
+
+
+const list = async (req, res, next) => {
+    try {
+        let isCheck = null;
+        // Lọc theo user_id
+        let idUserSearch = req.query.user_id;
+        console.log(idUserSearch);
+        if(typeof(idUserSearch) != 'undefined') {
+            isCheck = {user_id: idUserSearch}
+            console.log(isCheck);
+        }
+
+        let listOrder = await mdOrder.order.find(isCheck);
+        let clonedOrder = JSON.parse(JSON.stringify(listOrder)); // Nhân bản list order mới có thể thay đổi dữ liệu
+
+        
+
+
+        for (let index = 0; index < clonedOrder.length; index++) {
+            const order = clonedOrder[index];
+            let listOrderDetail = await mdOrderDetail.orderDetail
+                .find({ order_id: order._id })
+                .populate("product_id");
+
+            order.dataOrderDetail = listOrderDetail;
+        }
+
+
+        if (listOrder.length > 0) {
+            res.status(200).json({
+                status: 200,
+                message: 'Get data successfully!',
+                data: clonedOrder
+            });
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: 'No data!'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            code: 500,
+            message: error.message
+        });
+    }
+}
 
 const create = async (req, res, next) => {
     try {
@@ -10,7 +59,7 @@ const create = async (req, res, next) => {
             status
         } = req.body;
 
-        let objOrder = new md.order({
+        let objOrder = new mdOrder.order({
             user_id,
             address,
             phone_number,
@@ -37,4 +86,5 @@ const create = async (req, res, next) => {
 
 module.exports = {
     create,
+    list
 }
--- routes/order.route.js
+++ routes/order.route.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var orderController = require('../controllers/order.controller');
 
+router.get('/', orderController.list);
 router.post('/create', orderController.create);
 
 module.exports = router;
